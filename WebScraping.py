9317ec746685f8df27fd7534cb000619f0602d15
Grace Liu <jiayuan.liu@outlook.com>
2020-08-18 21:32:24 +0800

    calculated returns and risks of 3 stocks

--- WebScraping.py
+++ WebScraping.py
@@ -7,10 +7,18 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2020,5,1)
+start = dt.datetime(2018,1,1)
 end = dt.datetime.now()
 
 df = web.DataReader('BABA','yahoo',start,end)
 print(df.head())
 
+#df.to_csv('StockBABA.csv')
+
+#import datetime index
+#df = pd.read_csv('StockBABA.csv',parse_dates=True, index_col=0)
+#print(df.head())
+
+#df.plot()
+#plt.show()
 
